2a045a6f54d8e384cc001f9c733f226bd4005230
Perry <perry.vangronsveld@student.pxl.be>
2020-05-10 17:27:24 +0200

    finished oefening 2a

---
+++ oefening2/src/Util/Date.php
@@ -0,0 +1,55 @@
+<?php namespace Util;
+
+final class Date
+{
+    private $day, $month, $year;
+    private static $MONTHS = array("jan", "feb", "maa", "apr", "mei", "jun", "jul", "aug", "sep", "okt",
+        "nov", "dec");
+
+    public function __construct($day = 1, $month = 1, $year = 2008)
+    {
+        $this->day = $day;
+        $this->month = $month;
+        $this->year = $year;
+    }
+
+    public function print()
+    {
+        print ($this->day . '/' . $this->month . '/' . $this->year);
+    }
+
+    public function printMonth()
+    {
+        print ($this->day . '/' . self::$MONTHS[$this->month - 1] . '/' . $this->year);
+    }
+
+    public function getDay(): int
+    {
+        return $this->day;
+    }
+
+    public function setDay(int $day)
+    {
+        $this->day = $day;
+    }
+
+    public function getMonth(): int
+    {
+        return $this->month;
+    }
+
+    public function setMonth(int $month)
+    {
+        $this->month = $month;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year)
+    {
+        $this->year = $year;
+    }
+}
---
+++ oefening2/src/verwerk.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'Util/Date.php';
+use Util\Date;
+$day = $_POST['day'];
+$month = $_POST['month'];
+$year = $_POST['year'];
+$date = new Date($day, $month, $year);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Verwerk</title>
+</head>
+<body>
+<h1><?php
+    $date->printMonth();
+    ?></h1>
+</body>
+</html>
